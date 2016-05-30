8151806c18e6baa83de7192f527c6f06b6759ce0
GKrus <gherazavr@yandex.ru>
2016-05-30 22:09:01 +0300

    v 1.0
    Версия от 30.05_22:00

---
+++ project/project/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HelixToolkit;
+using HelixToolkit.Wpf;
+using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+using System.Windows.Threading;
+
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double sharp;
+        double side;
+        double up;
+        double back;
+        double stif;
+        double filling;
+        double flags;
+        double sl1;
+        double sl2;
+        double sl3;
+        double sl4;
+        double sl5;
+        double sl6;
+        double sl7;
+        double sl8;
+        double sl9;
+        double sl10;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Create3DViewPort();
+        }
+
+        //private void Create3DViewPort() { var hVp3D = new HelixViewport3D(); }
+        private void Create3DViewPort()
+        {
+            var hVp3D = new HelixViewport3D();
+            var lights = new DefaultLights();
+            var teaPot = new Teapot();
+            hVp3D.Children.Add(lights);
+            hVp3D.Children.Add(teaPot);
+           // this.AddChild(hVp3D);
+            
+        }
+        // example from zip
+        //private ModelVisual3D CreateDice()
+        //{
+        //    var diceMesh = new MeshBuilder();
+        //    diceMesh.AddBox(new Point3D(0, 0, 0), 1, 1, 1);
+        //    for (int i = 0; i < 2; i++)
+        //        for (int j = 0; j < 2; j++)
+        //            for (int k = 0; k < 2; k++)
+        //            {
+        //                var points = new List<Point3D>();
+        //                diceMesh.ChamferCorner(new Point3D(i - 0.5, j - 0.5, k - 0.5), 0.1, 1e-6, points);
+        //                //foreach (var p in points)
+        //                //    b.ChamferCorner(p, 0.03);
+        //            }
+
+        //    return new ModelVisual3D { Content = new GeometryModel3D { Geometry = diceMesh.ToMesh(), Material = Materials.White } };
+        //}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //расчет коэфицоентов 
+            double max = 0;
+            if (sl1 > sl2) max = sl1;
+            else max = sl2;
+            if (sl3 > max) max = sl3;
+            if (sl4 > sl3) max = sl4;
+            if (sl5 > sl4) max = sl5;
+            if (max - sl1 < 5 && max - sl2 < 5 && max - sl3 < 5 && max - sl4 < 5 && max - sl5 < 5)
+            {
+                sharp = (sl1 + sl2 + sl3 + sl4 + sl5) / 5;
+                //if small
+            } 
+            else
+            {
+                sharp = max - (sl1 + sl2 + sl3 + sl4 + sl5) / 10;
+                //sharp = (sl1 + sl2 + sl3 + sl4 + sl5) / 5;
+            } // sharp -- ok
+            up = (sl6*2 + sl3 + sl7 + sl9*2) / 6;//ok?
+            back = sl8;//ok
+            stif = (sharp*1.3 + sl5*2) / 3.3;//ok
+            filling = (sharp + sl7) / 2;//ok?
+            flags = sl7;//ok
+            side = sl10;//ok
+            //side = Slider.NameProperty.Get
+            string i = Convert.ToString(up);
+            MessageBox.Show(i);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl1 = ((Slider)sender).Value;
+            
+           
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl2 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_2(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl3 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_3(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl4 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_4(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl5 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_5(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl6 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_6(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl7 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_7(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl8 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_8(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl9 = ((Slider)sender).Value;
+        }
+
+        private void Slider_ValueChanged_9(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sl10 = ((Slider)sender).Value;
+        }
+    }
+}
