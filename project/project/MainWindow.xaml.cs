3b63692acd3db3213c6ded2f8a019156fd5d8da3
GKrus <gherazavr@yandex.ru>
2016-06-03 14:47:36 +0300

    v 1.5
    практика 03.06

--- project/project/MainWindow.xaml.cs
+++ project/project/MainWindow.xaml.cs
@@ -31,16 +31,25 @@ namespace project
         double stif;
         double filling;
         double flags;
+        ThreePointLights light;
+        SunLight sun_light;
+        int i;
        
 
         public MainWindow()
         {
             InitializeComponent();
-
+            i = 0;
+            light = new ThreePointLights();
+            nose_com.IsReadOnly = true;
            // var viewPort3d = new HelixViewport3D();
+            //var studio_light = new DefaultLights();
+            //studio_light.Brightness = 0.3;
+            //studio_light.Position = new Point3D(-5, -5, 5);
             
-
             //viewPort3d.Children.Add(new Teapot());
+            sun_light = new SunLight();
+            viewPort3d.Children.Add(sun_light);
 
             ModelVisual3D device3D = new ModelVisual3D();
             device3D.Content = Display3d(MODEL_PATH);
@@ -52,7 +61,7 @@ namespace project
             //viewPort3d.Children.Add(cube);
         }
 
-        //------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -73,26 +82,46 @@ namespace project
                 sharp = max - (nose.Value + chin.Value + eyes.Value + lips.Value + skin.Value) / 10;
                 //sharp = (sl1 + sl2 + sl3 + sl4 + sl5) / 5;
             } // sharp -- ok
-            up = (forehead.Value*2 + eyes.Value + cheelbones.Value + face_width.Value*2) / 6;//ok?
+            up = (forehead.Value*2 + eyes.Value + cheelbones.Value + face_width.Value*2 + sharp*2) / 8;//ok?
             back = ears.Value;//ok
             stif = (sharp*1.3 + skin.Value*2) / 3.3;//ok
             filling = (sharp + cheelbones.Value) / 2;//ok?
             flags = cheelbones.Value;//ok
             side = face_length.Value;//ok
-            var lights = new DirectionalHeadLight();
 
-            lights.Color = Colors.White;
-            lights.Brightness = 1;
-            lights.Position = new Point3D(5, -5, 5);
-            //viewPort3d.Children.Add(lights);
-            //side = Slider.NameProperty.Get
-            //string i = Convert.ToString(sharp);
-            //MessageBox.Show(i);
+            if (back > 5) light.KeyToFillLightRatio = 1 / 100;
+            else light.KeyToFillLightRatio = 1 / 0.9;
+           
+            if (filling > 5)
+            {
+                light.KeyLightBrightness = 0.7;
+                light.KeyToRimLightRatio = 1 / 1;
+            }
+            if (side > 5)
+            {
+                light.KeyLightSideAngle = light.KeyLightSideAngle - side * 2;
+            }
+            else
+            {
+                light.KeyLightSideAngle = light.KeyLightSideAngle + 10 - side * 2;
+            }
+            if (up > 5)
+            {
+                light.KeyLightAngle -= up * 2;
+            }
+            else
+            {
+                light.KeyLightAngle += 10 - up * 2;
+            }
 
-            //------------------------------------------------------------------------------------------------------
-
-
+           
+            
+            //MessageBox.Show("Ok");
         }
+//---------------------------------------------------------------------------------------------------------------------
+
+
+        
         // имопрт модели 
 
         private const string MODEL_PATH = "Female Head.obj";
@@ -136,61 +165,105 @@ namespace project
 
         private void def_light_Click(object sender, RoutedEventArgs e)
         {
-            var light = new ThreePointLights();
+            if (i <= 0)
+            {            
+                viewPort3d.Children.Remove(sun_light);
+                light.KeyLightBrightness = 0.8;
+                light.KeyLightSideAngle = 30;//ok
+                light.KeyLightAngle = 115;//ok
+                light.KeyToFillLightRatio = 1 / 1;
+                light.FillLightAngle = 310;
+                light.FillLightSideAngle = 310;
+                light.RimLightAngle = 30;
+                light.KeyToRimLightRatio = 1 / 1.7;
+                light.ShowLights = true;
+                viewPort3d.Children.Add(light);
+                i++;
+            }
+            else 
+            {
+                light.KeyLightBrightness = 0.8;
+                light.KeyLightSideAngle = 30;//ok
+                light.KeyLightAngle = 115;//ok
+                light.KeyToFillLightRatio = 1 / 1;
+                light.FillLightAngle = 310;
+                light.FillLightSideAngle = 310;
+                light.RimLightAngle = 30;
+                light.KeyToRimLightRatio = 1 / 1.5;
+                light.ShowLights = true;
+            }
 
-            //light.FillLightSideAngle = 180;
-            //light.FillLightAngle = 0;
-            light.KeyLightSideAngle = 30;//ok
-            light.KeyLightAngle = 115;//ok
-            light.KeyToFillLightRatio = 1 / 2;
-            light.FillLightAngle = 115;
-            light.FillLightSideAngle = 0;
-
-            light.KeyToRimLightRatio = 1 / 1000;
-            light.ShowLights = true;
-            //light.Brightness = 1;
-            //light.Position = new Point3D(-5, -5, 5);
-            viewPort3d.Children.Add(light);
         }
-    }
 
-    public class MainViewModel
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
-        /// </summary>
-        public MainViewModel()
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            // Create a model group
-            var modelGroup = new Model3DGroup();
-
-            // Create a mesh builder and add a box to it
-            var meshBuilder = new MeshBuilder(false, false);
-            meshBuilder.AddBox(new Point3D(0, 0, 1), 1, 2, 0.5);
-            meshBuilder.AddBox(new Rect3D(0, 0, 1.2, 0.5, 1, 0.4));
-
-            // Create a mesh from the builder (and freeze it)
-            var mesh = meshBuilder.ToMesh(true);
-
-            // Create some materials
-            var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
-            var redMaterial = MaterialHelper.CreateMaterial(Colors.Red);
-            var blueMaterial = MaterialHelper.CreateMaterial(Colors.Blue);
-            var insideMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
-
-            // Add 3 models to the group (using the same mesh, that's why we had to freeze it)
-            modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Material = greenMaterial, BackMaterial = insideMaterial });
-            modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(-2, 0, 0), Material = redMaterial, BackMaterial = insideMaterial });
-            modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(2, 0, 0), Material = blueMaterial, BackMaterial = insideMaterial });
-
-            // Set the property, which will be bound to the Content property of the ModelVisual3D (see MainWindow.xaml)
-            this.Model = modelGroup;
+            nose.Value = 10;
+            chin.Value = 10;
+            eyes.Value = 10;
+            lips.Value = 10;
+            skin.Value = 10;
+            forehead.Value = 10;
+            cheelbones.Value = 10;
+            ears.Value = 10;
+            face_length.Value = 10;
+            face_width.Value = 10;
         }
 
-        /// <summary>
-        /// Gets or sets the model.
-        /// </summary>
-        /// <value>The model.</value>
-        public Model3D Model { get; set; }
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            nose_tick.Visibility = Visibility.Visible;
+            nose_com.Visibility = Visibility.Visible;
+
+            if (nose.Value < 3)
+                nose_com.Text = "С этим носом будет минимум проблем";
+            else
+            {
+                nose_com.Text = "Обратите внимание на тени от носа, они могут Вас не обрадовать";
+                if (nose.Value > 6) nose_com.Text = "Тени от носа Вас не обрадуют, вероятно";
+            }
+            //MessageBox.Show("Ok");
+        }
     }
+
+    // доп  класс
+
+    //public class MainViewModel
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+    //    /// </summary>
+    //    public MainViewModel()
+    //    {
+    //        // Create a model group
+    //        var modelGroup = new Model3DGroup();
+
+    //        // Create a mesh builder and add a box to it
+    //        var meshBuilder = new MeshBuilder(false, false);
+    //        meshBuilder.AddBox(new Point3D(0, 0, 1), 1, 2, 0.5);
+    //        meshBuilder.AddBox(new Rect3D(0, 0, 1.2, 0.5, 1, 0.4));
+
+    //        // Create a mesh from the builder (and freeze it)
+    //        var mesh = meshBuilder.ToMesh(true);
+
+    //        // Create some materials
+    //        var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
+    //        var redMaterial = MaterialHelper.CreateMaterial(Colors.Red);
+    //        var blueMaterial = MaterialHelper.CreateMaterial(Colors.Blue);
+    //        var insideMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
+
+    //        // Add 3 models to the group (using the same mesh, that's why we had to freeze it)
+    //        modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Material = greenMaterial, BackMaterial = insideMaterial });
+    //        modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(-2, 0, 0), Material = redMaterial, BackMaterial = insideMaterial });
+    //        modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(2, 0, 0), Material = blueMaterial, BackMaterial = insideMaterial });
+
+    //        // Set the property, which will be bound to the Content property of the ModelVisual3D (see MainWindow.xaml)
+    //        this.Model = modelGroup;
+    //    }
+
+    //    /// <summary>
+    //    /// Gets or sets the model.
+    //    /// </summary>
+    //    /// <value>The model.</value>
+    //    public Model3D Model { get; set; }
+    //}
 }
